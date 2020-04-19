9b3288c958bf659fc075099c2dc05fd9191d3799
Sam O'Connell <samuel.oconnell1493@gmail.com>
2020-04-19 16:15:59 -0400

    Update

---
+++ bmdb-console/src/business/Actor.java
@@ -0,0 +1,67 @@
+package business;
+
+public class Actor {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private String birthdate;
+	
+	public Actor() {
+		super();
+	}
+	
+	
+	public Actor(int id, String firstName, String lastName, String gender, String birthdate) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.birthdate = birthdate;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Actor [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", gender=" + gender
+				+ ", birthdate=" + birthdate + "]";
+	}
+	
+	
+	
+	
+
+}
